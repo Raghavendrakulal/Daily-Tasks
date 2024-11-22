78d8e8186d30f94a1a3bfd71230c5250094e493c
Raghavendra <kulalraghavendra4@gmail.com>
2024-11-22 18:26:58 +0530

    mergesort

--- mergesort.java
+++ mergesort.java
@@ -1,8 +1,7 @@
-
 import java.util.Arrays;
 
+ public class mergesort{
 
-public class mergesort {
 
     public static void main(String[] args) {
         int[] arr = {9, 8, 7, 6, 5, 3, 4, 1, 2};
@@ -11,7 +10,7 @@ public class mergesort {
     }
 
     public static int[] mergesort(int[] arr) {
-        if (arr.length == 1) {
+        if (arr.length <= 1) {
             return arr;
         }
         int mid = arr.length / 2;
@@ -22,35 +21,27 @@ public class mergesort {
 
     public static int[] merge(int[] first, int[] second) {
         int[] mix = new int[first.length + second.length];
-        // int i=0;int j=0; int k=0;
         int i = 0, j = 0, k = 0;
+
+       
         while (i < first.length && j < second.length) {
             if (first[i] < second[j]) {
-                mix[k] = first[i];
-                i++;
-                
-
+                mix[k++] = first[i++];
+            } else {
+                mix[k++] = second[j++];
             }
-            else{
-            mix[k]=second[j];
-            j++;
-            }
-            k++;
-            
-            
-        
+        }
 
+       
+        while (i < first.length) {
+            mix[k++] = first[i++];
         }
-        while(i<first.length){
-        mix[k]=first[i];
-        i++;
-        k++;
+
+       
+        while (j < second.length) {
+            mix[k++] = second[j++];
         }
-        while(j<second.length){
-        mix[k]=second[i];
-        j++;
-        k++;
-        }
+
         return mix;
     }
 }
