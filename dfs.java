2541c39e4c5b0390f3e06ce8b9300c9fa59a78e3
Raghavendra <kulalraghavendra4@gmail.com>
2024-11-23 21:30:13 +0530

    small change

--- dfs.java
+++ dfs.java
@@ -15,7 +15,7 @@ public class dfs{
     }
 
     public ArrayList<Integer> dfsofgraph(int V,ArrayList<ArrayList<Integer>> adj){
-    boolean visited= new Boolean[V+1];
+    boolean[] visited= new boolean[V+1];
     visited[0]=true;
     ArrayList<Integer> list= new ArrayList<>();
     dfs(0,visited,adj,list);
